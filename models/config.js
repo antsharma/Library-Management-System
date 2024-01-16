9325998a976fb9eb9ff6c4b64c028088d4e84ab5
Anant <antsharma02@gmail.com>
2024-01-17 00:09:01 +0530

    database issue solved

--- models/config.js
+++ models/config.js
@@ -1,9 +1,17 @@
 var mysql      = require('mysql');
+//const url = process.env.
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'library_management_system'
+//   host     : 'localhost',
+//   user     : 'root',
+//   password : '',
+//   database : 'library_management_system'
+host: process.env.DB_HOST, 
+    user: process.env.DB_USERNAME, 
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DBNAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
 module.exports = {
